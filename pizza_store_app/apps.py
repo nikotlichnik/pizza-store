00078aa01b25029509b300c0f4e1aea4dca003d5
nikotlichnik <shtreisnikita@gmail.com>
2020-02-05 00:37:36 +0300

    added signals of creating user

--- pizza_store_app/apps.py
+++ pizza_store_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PizzaStoreAppConfig(AppConfig):
     name = 'pizza_store_app'
     verbose_name = 'Заказ пиццы'
+
+    def ready(self):
+        import pizza_store_app.signals
